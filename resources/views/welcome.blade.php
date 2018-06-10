9db052c499a6b41adb04944e268be70e4a14693f
Evan Burrell <burrelle@oregonstate.edu>
2018-06-09 20:39:36 -0700

    Flex your grid

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,21 +10,53 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link rel="stylesheet" href="css/normalize.css">
 </head>
+
 <body>
     <header>
-        <div class="container">
-            <div class="header-top">
-                <h1>Logo</h1>
-                <a href="#">Sign In</a>
-            </div>
+        <div class="section">
+            <div class="container">
+                <div class="header-top">
+                    <h1>Logo</h1>
+                    <a href="#">Sign In</a>
+                </div>
 
-            <nav>
-                <a href="#">Catalog</a>
-                <a href="#">Series</a>
-                <a href="#">Podcast</a>
-                <a href="#">Discussions</a>
-            </nav>
+                <nav>
+                    <a href="#">Catalog</a>
+                    <a href="#">Series</a>
+                    <a href="#">Podcast</a>
+                    <a href="#">Discussions</a>
+                </nav>
+            </div>
         </div>
     </header>
+
+    <div class="section">
+        <div class="container">
+            <!-- Four -->
+            <div class="row">
+                <div class="col">
+                    <div class="box">
+                        Some callout text
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="box">
+                        Some callout text
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="box">
+                        Some callout text
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="box">
+                        Some callout text
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
