2dcf52d2ebf288e40077b848e2e12baeb71a8f05
Evan Burrell <burrelle@oregonstate.edu>
2018-06-11 21:18:18 -0700

    Card design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,52 +7,35 @@
     <title>CSS For Backend Devs</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" media="screen" href="css/app.css" />
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
     <link rel="stylesheet" href="css/normalize.css">
 </head>
 
 <body>
-    <header>
-        <div class="section">
-            <div class="container">
-                <div class="header-top">
-                    <h1>Logo</h1>
-                    <a href="#">Sign In</a>
-                </div>
-
-                <nav>
-                    <a href="#">Catalog</a>
-                    <a href="#">Series</a>
-                    <a href="#">Podcast</a>
-                    <a href="#">Discussions</a>
-                </nav>
-            </div>
+    <div class="card">
+        <!-- left -->
+        <div class="card-left">
+            <a href="#" class="card-skill">PHP</a>
+            <img src="https://laracasts.com/images/series/circles/testing-vue.png">
+            <span class="card-difficulty">Intermediate Difficulty</span>
         </div>
-    </header>
 
-    <div class="section">
-        <div class="container">
-            <!-- Four -->
-            <div class="row">
-                <div class="col">
-                    <div class="box">
-                        Some callout text
-                    </div>
+        <!-- right -->
+        <div class="card-right">
+            <h3 class="card-title"><a href="#">Testing Jargon</a></h3>
+            <p>
+                There's no two ways about it: terminology in the testing wrold is incredibly overwhelming. 
+                Lets's fix that! Bit by bit, we'll break all these confusing concepts down as best we can.
+            </p>
+
+            <div class="card-meta">
+                <div>
+                    <img src="https://image.flaticon.com/icons/svg/182/182321.svg">
+                    5 Lessons
                 </div>
-                <div class="col">
-                    <div class="box">
-                        Some callout text
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="box">
-                        Some callout text
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="box">
-                        Some callout text
-                    </div>
+                <div>
+                    <img src="https://image.flaticon.com/icons/svg/149/149313.svg">
+                    53:10 min
                 </div>
             </div>
         </div>
